74c48f92e39da11c138ec13476ab8816885ec6b8
SUNG WOOK HWANG <destiny0810@naver.com>
2021-06-24 13:21:32 +0900

    Add postal code modal and redux store by SUNG WOOK HWANG

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './features/counter/counterSlice';
 import reservationReducer from './features/reservation/reservationSlice';
 import diagnosisReducer from './features/diagnosis/diagnosisSlice';
+import memberReducer from './features/member/memberSlice';
 export default configureStore({
   reducer: {
     counter: counterReducer,
     reservation: reservationReducer,
     diagnosis: diagnosisReducer,
+    member: memberReducer,
   },
 });
