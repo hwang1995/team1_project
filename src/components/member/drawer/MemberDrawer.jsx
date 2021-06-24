87152cd8f251d817a207fbb9443add338567f5ba
hooonfgfg <ghdzzk@naver.com>
2021-06-24 10:10:32 +0900

    features-member

--- src/components/member/drawer/MemberDrawer.jsx
+++ src/components/member/drawer/MemberDrawer.jsx
@@ -6,6 +6,7 @@ import {
   InputLabel,
   Select,
   MenuItem,
+  Modal,
 } from '@material-ui/core';
 import StyledTypography from 'components/common/typography/StyledTypography';
 import DrawerHeader from 'components/common/drawer/DrawerHeader';
@@ -14,11 +15,15 @@ import useWindowSize from 'hooks/useWindowSize';
 import StyledInputBase from 'components/common/input/StyledInputBase';
 import ResponsiveContainer from 'components/common/container/ResponsiveContainer';
 import StyledButton from 'components/common/button/StyledButton';
+import DaumPostCode from 'react-daum-postcode';
+import MemberModal from '../modal/MemberModal';
 
 const MemberDrawer = ({ isOpened, setOpened }) => {
   const { breakpoint } = useWindowSize();
   const [selectVal, setSelectVal] = useState('');
   // const [isLoading, setLoading] = useState(false);
+  const [isOpenPost, setOpenPost] = useState(false);
+  const [isOpenModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     if (breakpoint !== undefined) {
@@ -26,11 +31,29 @@ const MemberDrawer = ({ isOpened, setOpened }) => {
     }
   }, [breakpoint]);
 
+  //fuction(open){function(e){}}
   const toggleDrawer = (open) => (e) => {
     if (e && e.type === 'keydown' && (e.key === 'Tab' || e.key === 'Shift')) {
       return;
     }
     setOpened(open);
+  };
+
+  //submit
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const member = {
+      memberEmail: event.target.memberEmail.value,
+      memberPassword: event.target.memberPassword.value,
+      memberName: event.target.memberName.value,
+      memberBirth: event.target.memberBirth.value,
+    };
+    console.log(member);
+  };
+
+  //도로명주소 api컴포넌트 open
+  const openPost = () => {
+    setOpenPost(true);
   };
 
   return (
@@ -41,170 +64,203 @@ const MemberDrawer = ({ isOpened, setOpened }) => {
         onOpen={toggleDrawer(true)}
         onClose={toggleDrawer(false)}
       >
-        <ResponsiveContainer breakpoint={breakpoint}>
-          <DrawerHeader breakpoint={breakpoint}>
-            <h1>직원 추가</h1>
-            <div>
-              <AiOutlineClose size={32} onClick={() => setOpened(false)} />
-            </div>
-          </DrawerHeader>
+        <form onSubmit={handleSubmit}>
+          <ResponsiveContainer breakpoint={breakpoint}>
+            <DrawerHeader breakpoint={breakpoint}>
+              <h1>직원 추가</h1>
+              <div>
+                <AiOutlineClose size={32} onClick={() => setOpened(false)} />
+              </div>
+            </DrawerHeader>
 
-          <Grid container spacing={1} style={{ padding: '1rem' }}>
-            <Grid
-              item
-              xs={3}
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-              }}
-            >
-              <StyledTypography variant="h6" component="h5" weight={5}>
-                직책
-              </StyledTypography>
-            </Grid>
-            <Grid item xs={9}>
-              <FormControl style={{ width: '100%' }} variant="outlined">
-                <InputLabel id="form-label">의사</InputLabel>
-                <Select
-                  labelId="form-label"
-                  id="select-label"
-                  label="직책"
-                  value={selectVal}
+            <Grid container spacing={1} style={{ padding: '1rem' }}>
+              <Grid
+                item
+                xs={3}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <StyledTypography variant="h6" component="h5" weight={5}>
+                  직책
+                </StyledTypography>
+              </Grid>
+              <Grid item xs={9}>
+                <FormControl style={{ width: '100%' }} variant="outlined">
+                  <InputLabel id="form-label">의사</InputLabel>
+                  <Select
+                    labelId="form-label"
+                    id="select-label"
+                    label="직책"
+                    value={selectVal}
+                  >
+                    <MenuItem value="의사" onClick={() => setSelectVal('의사')}>
+                      의사
+                    </MenuItem>
+                    <MenuItem
+                      value="간호사"
+                      onClick={() => setSelectVal('간호사')}
+                    >
+                      간호사
+                    </MenuItem>
+                    <MenuItem
+                      value="검사자"
+                      onClick={() => setSelectVal('검사자')}
+                    >
+                      검사자
+                    </MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid
+                item
+                xs={3}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <StyledTypography variant="h6" component="h5" weight={5}>
+                  이메일
+                </StyledTypography>
+              </Grid>
+              <Grid item xs={7}>
+                <StyledInputBase name="memberEmail" />
+              </Grid>
+              <Grid
+                item
+                xs={2}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <StyledButton bgColor="lightgray">중복 체크</StyledButton>
+              </Grid>
+              <Grid
+                item
+                xs={3}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <StyledTypography variant="h6" component="h5" weight={5}>
+                  비밀번호
+                </StyledTypography>
+              </Grid>
+              <Grid item xs={9}>
+                <StyledInputBase name="memberPassword" />
+              </Grid>
+              <Grid
+                item
+                xs={3}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <StyledTypography variant="h6" component="h5" weight={5}>
+                  이름
+                </StyledTypography>
+              </Grid>
+              <Grid item xs={9}>
+                <StyledInputBase name="memberName" />
+              </Grid>
+              <Grid
+                item
+                xs={3}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <StyledTypography variant="h6" component="h5" weight={5}>
+                  생년월일
+                </StyledTypography>
+              </Grid>
+              <Grid item xs={9}>
+                <StyledInputBase name="memberBirth" />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <StyledTypography variant="h6" component="h5" weight={5}>
+                  주소
+                </StyledTypography>
+              </Grid>
+
+              <Grid item xs={9}>
+                <StyledInputBase name="memberAddress1" readOnly />
+              </Grid>
+              <Grid
+                item
+                xs={3}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <StyledButton
+                  bgColor="lightgray"
+                  onClick={() => setOpenModal(true)}
                 >
-                  <MenuItem value="의사" onClick={() => setSelectVal('의사')}>
-                    의사
-                  </MenuItem>
-                  <MenuItem
-                    value="간호사"
-                    onClick={() => setSelectVal('간호사')}
-                  >
-                    간호사
-                  </MenuItem>
-                  <MenuItem
-                    value="검사자"
-                    onClick={() => setSelectVal('검사자')}
-                  >
-                    검사자
-                  </MenuItem>
-                </Select>
-              </FormControl>
+                  주소 찾기
+                </StyledButton>
+              </Grid>
+              <Grid item xs={12}>
+                <StyledInputBase name="memberAddress2" readOnly />
+              </Grid>
+              <Grid item xs={12}>
+                <StyledInputBase
+                  name="memberAddress3"
+                  placeholder="상세 주소를 입력하세요."
+                />
+              </Grid>
             </Grid>
-            <Grid
-              item
-              xs={3}
+            <div
               style={{
+                position: 'fixed',
+                bottom: 30,
+                right: 20,
                 display: 'flex',
-                alignItems: 'center',
+                justifyContent: 'flex-end',
               }}
             >
-              <StyledTypography variant="h6" component="h5" weight={5}>
-                이메일
-              </StyledTypography>
-            </Grid>
-            <Grid item xs={7}>
-              <StyledInputBase />
-            </Grid>
-            <Grid
-              item
-              xs={2}
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-              }}
-            >
-              <StyledButton bgColor="lightgray">중복 체크</StyledButton>
-            </Grid>
-            <Grid
-              item
-              xs={3}
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-              }}
-            >
-              <StyledTypography variant="h6" component="h5" weight={5}>
-                비밀번호
-              </StyledTypography>
-            </Grid>
-            <Grid item xs={9}>
-              <StyledInputBase />
-            </Grid>
-            <Grid
-              item
-              xs={3}
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-              }}
-            >
-              <StyledTypography variant="h6" component="h5" weight={5}>
-                이름
-              </StyledTypography>
-            </Grid>
-            <Grid item xs={9}>
-              <StyledInputBase />
-            </Grid>
-            <Grid
-              item
-              xs={3}
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-              }}
-            >
-              <StyledTypography variant="h6" component="h5" weight={5}>
-                생년월일
-              </StyledTypography>
-            </Grid>
-            <Grid item xs={9}>
-              <StyledInputBase />
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-              }}
-            >
-              <StyledTypography variant="h6" component="h5" weight={5}>
-                주소
-              </StyledTypography>
-            </Grid>
-
-            <Grid item xs={9}>
-              <StyledInputBase />
-            </Grid>
-            <Grid
-              item
-              xs={3}
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-              }}
-            >
-              <StyledButton bgColor="lightgray">주소 찾기</StyledButton>
-            </Grid>
-            <Grid item xs={12}>
-              <StyledInputBase />
-            </Grid>
-            <Grid item xs={12}>
-              <StyledInputBase />
-            </Grid>
-          </Grid>
-
-          <div
-            style={{
-              position: 'fixed',
-              bottom: 30,
-              right: 20,
-              display: 'flex',
-              justifyContent: 'flex-end',
-            }}
-          >
-            <StyledButton bgColor="lightgray">임직원 추가</StyledButton>
-          </div>
-        </ResponsiveContainer>
+              <StyledButton type="submit" bgColor="lightgray">
+                임직원 추가
+              </StyledButton>
+            </div>
+            <MemberModal
+              isOpenModal={isOpenModal}
+              setOpenModal={setOpenModal}
+            />
+            {/* <Modal open={isOpenModal} onClose={closeModal}>
+            <div>
+              <div>
+                Lorem, ipsum dolor sit amet
+                consectetur adipisicing elit. Modi atque voluptatem maxime et?
+                Explicabo, culpa tempora odio dicta at minima tempore ipsam
+                quisquam labore vitae similique saepe quam! Laboriosam, omnis.
+              </div>
+              <div>
+                {isOpenPost ? (
+                  <DaumPostCode
+                    style={postCodeStyle}
+                    onComplete={handleComplete}
+                  />
+                ) : null}
+              </div>
+            </div>
+          </Modal> */}
+          </ResponsiveContainer>
+        </form>
       </SwipeableDrawer>
     </Fragment>
   );
